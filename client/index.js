95dfd9badf55c15cb89194d790301b846344423c
ramilmustafin <ramilmustafin33@gmail.com>
2023-11-22 23:16:40 +0400

    code updated

--- client/index.js
+++ client/index.js
@@ -6,9 +6,16 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  
+  const merkleTree = new MerkleTree(niceList);
+  const name = 'Shelly Toy';
+  const proof = merkleTree.getProof(name);
+
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof
   });
 
   console.log({ gift });
